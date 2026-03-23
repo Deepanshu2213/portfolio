fcf6075ca622106e7dc3d203ee1b5153d984ed6d
Deepanshu <deepanshuhead200@gmail.com>
2026-03-23 14:41:45 +0530

    naming issue

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -13,7 +13,7 @@ const menu = {
   Experience: [<MdCardTravel />, 'experience'],
   Projects: [<GiFilmProjector />, 'project'],
   Contact: [<IoMdContact />, 'contract'],
-  CV: [<FaFileDownload />, 'Deepanshu_resume.pdf', true],
+  CV: [<FaFileDownload />, 'Deepanshu_Resume_10_03_1001.pdf', true],
 };
 const MenuBar = ({ setMenuBar, menuBar }) => {
   const menu = useRef();
